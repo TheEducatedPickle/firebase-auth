8d5ec1b435fad34cf64283c7d2f78888b27134d5
Austin Yuan <austinyuan2@gmail.com>
2019-05-09 18:17:41 -0700

    first

--- firebase.js
+++ firebase.js
@@ -1,7 +1,11 @@
-//Paste the details given to you by firebase under this comment
-//eg
-// var firebaseConfig = {
-// ...config
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+    apiKey: "AIzaSyDLj9Zlwr2-2SnXPaQL3ATDavr7-8z9qR4",
+    authDomain: "fir-auth-example-6cae7.firebaseapp.com",
+    databaseURL: "https://fir-auth-example-6cae7.firebaseio.com",
+    projectId: "fir-auth-example-6cae7",
+    storageBucket: "fir-auth-example-6cae7.appspot.com",
+    messagingSenderId: "979298614536",
+    appId: "1:979298614536:web:e86282daa48bf162"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- index.js
+++ index.js
@@ -19,13 +19,21 @@ function handleAuthChanges() {
 function handleSignIn() {
   const email= document.getElementById("email").value
   const pass = document.getElementById("password").value
-  console.log("Sign in" + email)
+  firebase.auth().signInWithEmailAndPassword(email,pass).catch(function(error) {
+    const errcode = error.errcode
+    const errmsg = error.message
+    alert(errmsg)
+  });
 }
 
 function handleSignUp() {
   const email = document.getElementById("email").value
   const pass = document.getElementById("password").value
-  console.log("Sign up" + email)
+  firebase.auth().createUserWithEmailAndPassword(email,pass).catch(function(error) {
+    const errcode = error.errcode
+    const errmsg = error.message
+    alert(errmsg)
+  });
 }
 
 window.onload = () => {
